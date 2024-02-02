c5df58de12c0653175a3513f671664dd17d63b87
mr-mensah <mr-mensah@gmail.com>
2024-02-02 19:55:54 +0000

    slmost

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -4,7 +4,7 @@ from flask import Blueprint
 
 app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
 
-# Wildcard import is still expected
+
 from api.v1.views.index import *
-from api.v1.views.amenities import *
-from api.v1.views.users import *
+from api.v1.views import states, cities, amenities, users
+from api.v1.views import places, places_reviews
