f114b81780c0c0c61f888fb1126737ed2223d7eb
mr-mensah <108750327+mr-mensah@users.noreply.github.com>
2024-02-03 14:57:09 +0000

    __init__.py

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
-""" init"""
+"""
+Init
+"""
+
 from flask import Blueprint
 
-app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
-
+app_views = Blueprint('app_view', __name__, url_prefix='/api/v1/')
 
 from api.v1.views.index import *
 from api.v1.views import states, cities, amenities, users
